eb338e78bd939a29f1f3d3568f442f784f5e01c1
Ibrahim Azhar Armar <azhar@iarmar.com>
2016-04-10 00:40:29 +0530

    Include PHP-CLI

--- scripts/php.sh
+++ scripts/php.sh
@@ -9,6 +9,7 @@ printf "\e[31m installing PHP..."
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 sudo yum -y install php70w
+sudo yum -y install php70w-cli
 sudo yum -y install php70w-common
 sudo yum -y install php70w-mysql
 sudo yum -y install php70w-intl
