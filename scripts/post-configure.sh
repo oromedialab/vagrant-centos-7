f986cdaa915f7af637ce483acd1323ce2967da90
Ibrahim Azhar Armar <azhar@iarmar.com>
2016-04-10 00:04:21 +0530

    Create DOCUMENT_ROOT directory if not exist

--- scripts/post-configure.sh
+++ scripts/post-configure.sh
@@ -5,6 +5,7 @@ sudo setenforce 0
 sudo sed -i 's/SELINUX=\(enforcing\|permissive\)/SELINUX=disabled/g' /etc/selinux/config
 
 # Change Apache DOCUMENT_ROOT and restart apache
+sudo mkdir -p $1
 sudo sed -i $"s#DocumentRoot \"/var/www/html\"#DocumentRoot \"$1\"#g" /etc/httpd/conf/httpd.conf
 sudo systemctl start httpd.service
 sudo systemctl restart httpd.service
