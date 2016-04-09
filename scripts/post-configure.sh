ec65e125c6b2e268f54981c19949835331b6ce7d
Ibrahim Azhar Armar <azhar@iarmar.com>
2016-04-09 21:44:03 +0530

    Restart and enable http service in post-configure

--- scripts/post-configure.sh
+++ scripts/post-configure.sh
@@ -3,3 +3,9 @@
 # Disable SELINUX
 sudo setenforce 0
 sudo sed -i 's/SELINUX=\(enforcing\|permissive\)/SELINUX=disabled/g' /etc/selinux/config
+
+# Change Apache DOCUMENT_ROOT and restart apache
+sudo sed -i 's#DocumentRoot "/var/www/html"#DocumentRoot "/var/www/html/centos"#g' /etc/httpd/conf/httpd.conf
+sudo systemctl start httpd.service
+sudo systemctl restart httpd.service
+sudo systemctl enable httpd.service
