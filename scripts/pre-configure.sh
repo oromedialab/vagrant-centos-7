b6509e904d089e2dad3dd1ab28d227d6d34a342a
Ibrahim Azhar Armar <azhar@iarmar.com>
2016-04-10 00:49:59 +0530

    Update yum and RHEL repo

--- scripts/pre-configure.sh
+++ scripts/pre-configure.sh
@@ -3,5 +3,9 @@
 # Set Language
 localectl set-locale LANG=en_US.utf8
 
-# Update yum
+# Update repositores
 sudo yum update
+sudo yum -y install epel-release
+sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+sudo rpm -Uvh http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
