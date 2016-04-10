677c14135b85ea59d4b63105bf286d97adc2b206
Ibrahim Azhar Armar <azhar@iarmar.com>
2016-04-10 10:44:00 +0530

    Disable RPM update

--- scripts/pre-configure.sh
+++ scripts/pre-configure.sh
@@ -5,7 +5,7 @@ localectl set-locale LANG=en_US.utf8
 
 # Update repositores
 sudo yum update
-sudo yum -y install epel-release
-sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
-sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-sudo rpm -Uvh http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+# sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+# sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+# sudo rpm -Uvh http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+# sudo yum -y install epel-release
