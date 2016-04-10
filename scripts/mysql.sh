95f43969b14b7f26dcdbad462da0debf871ecd35
Ibrahim Azhar Armar <azhar@iarmar.com>
2016-04-10 13:18:02 +0530

    Create database and user from cnfig file

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -9,4 +9,5 @@ printf "\e[31m installing mariadb..."
 sudo yum -y install mariadb-server mariadb
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
+mysql -uroot -e "CREATE DATABASE $3; CREATE USER '$1'@'localhost' IDENTIFIED BY '$2'; GRANT ALL PRIVILEGES ON * . * TO '$1'@'localhost'; FLUSH PRIVILEGES;"
 printf "\e[31m✔ mariadb installed!"
